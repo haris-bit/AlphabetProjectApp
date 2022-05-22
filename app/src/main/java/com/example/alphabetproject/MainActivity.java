a29a731e8ea38e199cbae5b216dfb6bd6ea087d2
haris <harisiftikhar109@gmail.com>
2022-05-22 16:38:37 +0500

    changes to UI

--- app/src/main/java/com/example/alphabetproject/MainActivity.java
+++ app/src/main/java/com/example/alphabetproject/MainActivity.java
@@ -3,12 +3,72 @@ package com.example.alphabetproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnA;
+    private Button btnB;
+    private Button btnC;
+    private Button btnD;
+    private Button btnE;
+    private Button btnF;
+    private Button btnG;
+    private Button btnH;
+    private Button btnI;
+    private Button btnJ;
+    private Button btnK;
+    private Button btnL;
+    private Button btnM;
+    private Button btnN;
+    private Button btnO;
+    private Button btnP;
+    private Button btnQ;
+    private Button btnR;
+    private Button btnS;
+    private Button btnT;
+    private Button btnU;
+    private Button btnV;
+    private Button btnW;
+    private Button btnX;
+    private Button btnY;
+    private Button btnZ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // binding
+        btnA = findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        btnD = findViewById(R.id.btnD);
+        btnE = findViewById(R.id.btnE);
+        btnF = findViewById(R.id.btnF);
+        btnG = findViewById(R.id.btnG);
+        btnH = findViewById(R.id.btnH);
+        btnI = findViewById(R.id.btnI);
+        btnJ = findViewById(R.id.btnJ);
+        btnK = findViewById(R.id.btnK);
+        btnL = findViewById(R.id.btnL);
+        btnM = findViewById(R.id.btnM);
+        btnN = findViewById(R.id.btnN);
+        btnO = findViewById(R.id.btnO);
+        btnP = findViewById(R.id.btnP);
+        btnQ = findViewById(R.id.btnQ);
+        btnR = findViewById(R.id.btnR);
+        btnS = findViewById(R.id.btnS);
+        btnT = findViewById(R.id.btnT);
+        btnU = findViewById(R.id.btnU);
+        btnV = findViewById(R.id.btnV);
+        btnW = findViewById(R.id.btnW);
+        btnX = findViewById(R.id.btnX);
+        btnY = findViewById(R.id.btnY);
+        btnZ = findViewById(R.id.btnZ);
+
+
+
     }
 }
