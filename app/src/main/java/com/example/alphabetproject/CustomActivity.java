bf7d5dbfd1eb8b06890fd8aeb7f1273ed3c635e9
haris <harisiftikhar109@gmail.com>
2022-06-05 11:56:20 +0500

    updated the custom listview

--- app/src/main/java/com/example/alphabetproject/CustomActivity.java
+++ app/src/main/java/com/example/alphabetproject/CustomActivity.java
@@ -20,10 +20,33 @@ public class CustomActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
 
         List<Alphabet> lst = new ArrayList<Alphabet>();
+
         lst.add(new Alphabet(R.drawable.a1, "A"));
         lst.add(new Alphabet(R.drawable.b1, "B"));
         lst.add(new Alphabet(R.drawable.c1, "C"));
         lst.add(new Alphabet(R.drawable.d1, "D"));
+        lst.add(new Alphabet(R.drawable.e1, "E"));
+        lst.add(new Alphabet(R.drawable.f1, "F"));
+        lst.add(new Alphabet(R.drawable.g1, "G"));
+        lst.add(new Alphabet(R.drawable.h1, "H"));
+        lst.add(new Alphabet(R.drawable.i1, "I"));
+        lst.add(new Alphabet(R.drawable.j1, "J"));
+        lst.add(new Alphabet(R.drawable.k1, "K"));
+        lst.add(new Alphabet(R.drawable.l1, "L"));
+        lst.add(new Alphabet(R.drawable.m1, "M"));
+        lst.add(new Alphabet(R.drawable.n1, "N"));
+        lst.add(new Alphabet(R.drawable.o1, "O"));
+        lst.add(new Alphabet(R.drawable.p1, "P"));
+        lst.add(new Alphabet(R.drawable.q1, "Q"));
+        lst.add(new Alphabet(R.drawable.r1, "R"));
+        lst.add(new Alphabet(R.drawable.s1, "S"));
+        lst.add(new Alphabet(R.drawable.t1, "T"));
+        lst.add(new Alphabet(R.drawable.u1, "U"));
+        lst.add(new Alphabet(R.drawable.v1, "V"));
+        lst.add(new Alphabet(R.drawable.w1, "W"));
+        lst.add(new Alphabet(R.drawable.x1, "X"));
+        lst.add(new Alphabet(R.drawable.y1, "Y"));
+        lst.add(new Alphabet(R.drawable.z1, "Z"));
 
 
         AlphabetAdapter alphabetAdapter = new AlphabetAdapter(this, lst);
